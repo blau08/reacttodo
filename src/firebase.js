7f57b02e4e26e221eaff69a2628d8cdf906d3c18
Brian Lau <blau08@gmail.com>
2024-09-03 23:17:24 -0700

    secure firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,18 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA-EU7xm84DIb_ZziJ8TnAlrlgLDyvMwyI",
-    authDomain: "brianlautodoapp.firebaseapp.com",
-    databaseURL: "https://brianlautodoapp-default-rtdb.firebaseio.com",
-    projectId: "brianlautodoapp",
-    storageBucket: "brianlautodoapp.appspot.com",
-    messagingSenderId: "979981406100",
-    appId: "1:979981406100:web:4667be5d60bfb10b770981"
-  };
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
